2de896b08c96d52668bdd25bcd142ba629e29eb3
Crystal Tenn <crtenn@microsoft.com>
2017-05-10 14:53:13 -0400

    edit holder object to new class

--- FlyingKetchup/Manager/SearchManager.cs
+++ FlyingKetchup/Manager/SearchManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlyingKetchup.Models;
 using Google.Apis.QPXExpress.v1;
 using Google.Apis.QPXExpress.v1.Data;
 using Google.Apis.Services;
@@ -14,23 +15,50 @@ namespace FlyingKetchup.Manager
         public void GetLowestRoundTripPrice(string departDate, string departLocation, string arrivalDate, string arrivalLocation)
         {
             // Call google API
-            var result = CallGoogleApi();
+            var results = CallGoogleApi();
+
+            List<HolderObject> allFlights = new List<HolderObject>();
+            int numberFlights = results.Trips.TripOption.Count;
+
+            foreach (var result in results.Trips.TripOption)
+            {
+                HolderObject holderObject = new HolderObject
+                {
+                    SaleTotal = result.SaleTotal,
+                    Origin = result.Pricing[1].Fare[1].Origin,
+                    Destination = result.Pricing.First().Fare.First().Destination,
+                    Carrier = result.Pricing.First().Fare.First().Carrier,
+                    ArrivalTime = result.Slice.First().Segment.First().Leg.First().ArrivalTime,
+                    DepartureTime = result.Slice.First().Segment.First().Leg.First().DepartureTime,
+                    Duration = result.Slice.First().Segment.First().Leg.First().Duration,
+                };
+
+                allFlights.Add(holderObject);
+            }
 
             // Hit repo layer to record price in general table of history of searches
 
             // return the google result
         }
 
+
+
         private TripsSearchResponse CallGoogleApi()
         {
             QPXExpressService service = new QPXExpressService(new BaseClientService.Initializer() { ApiKey = "AIzaSyCpjedj9wpLTgXxKMTquLYWtq0awsASYgQ", ApplicationName = "CodeFlyingKetchup", });
 
-            TripsSearchRequest x = new TripsSearchRequest();
-            x.Request = new TripOptionsRequest();
-            x.Request.Passengers = new PassengerCounts { AdultCount = 2 };
-            x.Request.Slice = new List<SliceInput>();
-            x.Request.Slice.Add(new SliceInput() { Origin = "JFK", Destination = "BOS", Date = "2017-10-29" });
-            x.Request.Solutions = 10;
+            TripsSearchRequest x = new TripsSearchRequest
+            {
+                Request = new TripOptionsRequest
+                {
+                    Passengers = new PassengerCounts {AdultCount = 2},
+                    Slice = new List<SliceInput>
+                    {
+                        new SliceInput() {Origin = "TPA", Destination = "SEA", Date = "2017-10-29"}
+                    },
+                    Solutions = 10
+                }
+            };
 
             return service.Trips.Search(x).Execute();
         }
---
+++ FlyingKetchup/Models/HolderObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlyingKetchup.Models
+{
+    public class HolderObject
+    {
+        public string Flight { get; set; }
+        public string SaleTotal { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Carrier { get; set; }
+        public string ArrivalTime { get; set; }
+        public string DepartureTime { get; set; }
+        public int? Duration { get; set; }
+    }
+}
